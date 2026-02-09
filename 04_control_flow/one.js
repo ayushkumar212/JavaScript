3bc8a719cd4c36f9aaee2ea4e9a260a1bd90bfd2
ayushkumar212 <212ayushkumar@gmail.com>
2026-02-09 23:07:23 +0530

    Add files via upload

---
+++ 04_control_flow/one.js
@@ -0,0 +1,105 @@
+//                         01 : if-else
+
+/*
+if statement - syntax
+
+if (condition) {
+    
+}else{
+    
+    }
+
+comparision operator - < , > , <= , >= , == , != , === , !==
+*/
+
+
+/*
+temperature = 41
+if (temperature ===40 ) {
+    console.log("less than 50");  
+}else{
+    console.log("temperature is greater than 50"); 
+}
+*/
+
+
+/*
+const score  = 200
+
+if (score > 100) {
+    let power = "fly"
+    console.log('user power : ${power}');   
+}
+
+console.log('user power : ${power}');
+*/
+
+
+/*
+Short if code
+This is not a good practice of writing code with Comma . 
+const balance = 1000
+if (balance > 500 ) console.log("test"); , console.log("test2");
+*/
+
+
+//          Nested loops : if - else if - else
+
+/*
+const balance = 1000
+if (balance < 500) {
+    console.log("less than 500"); 
+}else if(balance < 750){
+    console.log("less than 750");
+}else if(balance < 900){
+    console.log("less than 900");
+}else{
+  console.log("less than 1200");
+    
+}
+*/
+
+
+/*
+const userLoggedIn = true
+const debitcard = true
+const loggedInFromGoogle = true
+const loggedInFromEmail = false
+
+if (userLoggedIn && debitcard ) {
+    console.log("Allow to buy course");   
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+   console.log("user logged in ");
+    
+}
+*/
+
+
+/*
+
+
+      //   Nullish Coalescing Operator (??) : null undefined  
+
+let val1;
+val1 = 5 ?? 10
+val1 = null ?? 10
+val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20 
+
+console.log(val1);
+
+*/
+
+
+/*
+
+//                       Terniary Operator 
+
+Syntax : condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
+
+*/
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,35 @@
+/*
+
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got user email");  
+}else{
+    console.log("don't have user  email");   
+}
+
+if (userEmail.length === 0) {
+    console.log("array is empty");
+    
+}
+
+const emptyobj = {}
+
+if (Object.keys(emptyobj).length ===  0) {
+    console.log("object is empty");
+    
+}
+
+*/ 
+
+/*
+
+                  Falsy values
+
+false , 0 , -0 , BigInt 0n , "" , null , undefined , Nan
+
+                 Truthy values
+
+"0" , 'false' , " " ,[] , {}  , function(){} 
+
+*/
